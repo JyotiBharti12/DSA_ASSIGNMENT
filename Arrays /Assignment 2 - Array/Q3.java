784db7c8f42057ee0345cc146004b77d1db3ea2a
Jyoti Bharti <122717666+JyotiBharti12@users.noreply.github.com>
2023-07-10 21:44:40 +0530

    Add files via upload

---
+++ Arrays /Assignment 2 - Array/Q3.java
@@ -0,0 +1,36 @@
+import java.util.HashMap; 
+import java.util.Map;
+ 
+public class LongestHarmoniousSubsequence 
+{ 
+	public int findLHS(int[] nums) 
+	{
+		Map<Integer, Integer> freqMap = new HashMap<>(); 
+		int longestSubsequenceLength = 0;
+		 
+		// Count the frequency of each number in the array 
+		for (int num : nums) 
+		{
+		freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+		}
+	// Iterate over the numbers and check for harmonious 
+		subsequence for (int num : freqMap.keySet()) 
+		{
+			if (freqMap.containsKey(num + 1)) 
+			{
+				int subsequenceLength = freqMap.get(num) + freqMap.get(num + 1); longestSubsequenceLength = Math.max(longestSubsequenceLength,
+				subsequenceLength);
+			}
+		}
+	
+	return longestSubsequenceLength;
+	}
+	 
+	public static void main(String[] args) 
+	{ 
+	int[] nums = {1, 3, 2, 2, 5, 2, 3, 7};
+	LongestHarmoniousSubsequence solution = new LongestHarmoniousSubsequence(); int longestSubsequenceLength = solution.findLHS(nums); System.out.println("Longest harmonious subsequence length: " +
+	longestSubsequenceLength);
+	}
+} 
+
